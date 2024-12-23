5a8d96100bd47d35bc15a02e3b93d7566f94ce9b
seyitkadir <seyitkadir15@gmail.com>
2024-12-23 21:07:00 +0300

    VHDL2019-3:: Code Update

--- SEYT/SubprogramExample.vhd
+++ SEYT/SubprogramExample.vhd
@@ -1,29 +1,30 @@
-library IEEE; 
-use IEEE.STD_LOGIC_1164.ALL; 
-use IEEE.NUMERIC_STD.ALL; 
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-procedure UpdateValue(signal clk : in STD_LOGIC; signal reset : in STD_LOGIC; 
-			signal data_in : in STD_LOGIC_VECTOR(7 downto 0); 
-			signal data_out : out STD_LOGIC_VECTOR(7 downto 0)) is 
-Begin
-	 if reset = '1' then 
-		data_out <= (others => '0’); 
-	elsif rising_edge(clk) then 
-		data_out <= data_in; 
-	end if; 
+procedure updatevalue(signal clk : in std_logic; signal reset : in std_logic; 
+            signal data_in : in std_logic_vector(7 downto 0); 
+            signal data_out : out std_logic_vector(7 downto 0)) is 
+begin
+    if reset = '1' then 
+        data_out <= (others => '0'); 
+    elsif rising_edge(clk) then 
+        data_out <= data_in; 
+    end if; 
 end procedure; 
-entity ProcedureWithClock is 
-     Port ( 
-          clk : in STD_LOGIC; 
-          reset : in STD_LOGIC; 
-          data_in : in STD_LOGIC_VECTOR(7 downto 0); 
-          data_out : out STD_LOGIC_VECTOR(7 downto 0) ); 
-end ProcedureWithClock; 
-architecture Behavioral of ProcedureWithClock is 
+
+entity procedurewithclock is 
+     port ( 
+          clk : in std_logic; 
+          reset : in std_logic; 
+          data_in : in std_logic_vector(7 downto 0); 
+          data_out : out std_logic_vector(7 downto 0) ); 
+end procedurewithclock; 
+
+architecture behavioral of procedurewithclock is 
 begin 
             process(clk, reset) 
             begin  
-            UpdateValue(clk, reset, data_in, data_out); 
+            updatevalue(clk, reset, data_in, data_out); 
             end process; 
-end Behavioral;
-
+end behavioral;
