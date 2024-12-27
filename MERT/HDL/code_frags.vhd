5bb9653b7f459227b7a68d6dbcc43f5537f092dc
MertEcevit-ops <mertecevit@protonmail.com>
2024-12-27 23:23:15 +0300

    VHDL2019-5 Final Code Parts

--- MERT/HDL/code_frags.vhd
+++ MERT/HDL/code_frags.vhd
@@ -2,17 +2,61 @@
 
 --DECLARATIONS EXAMPLES
 --Type declarations
-type boolean is (false, true);
-type bit_vector is array (7 downto 0) of bit;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
---Subtype declarations
-subtype my_vector is std_logic_vector (3 downto 0);
-subtype word is unsigned(15 downto 0);
+package custom is
+    type boolean is (false, true);
+    type bit_vector is array (7 downto 0) of bit; --type declarations
 
---Object declarations
-constant loop_number : integer := 7;    --constant declaration
-signal sum, carry1, carry2 : bit;       --signal declaration
-variable Carry: STD_LOGIC := '0';       --variable declaration
+    subtype my_vector is std_logic_vector (3 downto 0);
+    subtype word is unsigned(15 downto 0); --subtype declarations
+end package custom;
+
+entity example is
+    Port (
+        clk : in std_logic;
+        reset : in std_logic;
+        result : out std_logic
+    );
+end example;
+
+architecture bhv of example is
+    -- Constant declaration
+    constant loop_number : integer := 7;
+    -- Signal declarations
+    signal sum, carry1, carry2 : bit := '0';
+    -- Variable declaration (used in a process)
+    signal Carry : std_logic := '0';
+begin
+    process(clk, reset)
+        variable temp_result : std_logic := '0';
+    begin
+        if reset = '1' then
+            sum <= '0';
+            carry1 <= '0';
+            carry2 <= '0';
+            Carry <= '0';
+            temp_result := '0';
+            result <= '0';
+        elsif rising_edge(clk) then
+            for i in 0 to loop_number loop
+                if i mod 2 = 0 then
+                    Carry <= '1';
+                    temp_result := Carry;
+                else
+                    Carry <= '0';
+                    temp_result := Carry;
+                end if;
+            end loop;
+            sum <= '1';
+            carry1 <= bit(Carry);
+            carry2 <= '1';
+            result <= temp_result;
+        end if;
+    end process;
+end bhv;
+
 
 --interface declarations
 type Axi4LiteWriteAddressType is record
@@ -36,45 +80,92 @@ AxiResponder1  : view Axi4LiteResponderView ;
 AxiResponder2  : view Axi4LiteResponderView ;  --Use the interface in an entity declaration
 
 --alias declaration
-constant tc : time := 2.5 ns ;
-alias delay : time IS tc ;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity TimingExample is
+    Port (
+        clk : in std_logic;
+        reset : in std_logic;
+        signal_in : in std_logic;
+        signal_out : out std_logic
+    );
+end TimingExample;
+
+architecture Behavioral of TimingExample is
+    -- Constant declaration
+    constant tc : time := 2.5 ns;
+    -- Alias declaration
+    alias delay : time is tc;
+begin
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            signal_out <= '0' after delay;
+        elsif rising_edge(clk) then
+            signal_out <= signal_in after delay;
+        end if;
+    end process;
+end Behavioral;
+
 
 
 --attribute declarations
-type T is (A, B, C, D, E);
-subtype S is T range D downto B;
-S'left = D
-S'right = B
-S'low = B
-S'high = D
-S'base'left = A
-T'ascending = TRUE
-S'ascending = FALSE
-T'image(A) = "a"
-T'value("E") = E
-T'pos(A) = 0
-S'pos(B) = 1
-T'val(4) = E
-S'succ(B) = C
-S'pred(C) = B
-S'leftof(B) = C
-S'rightof(C) = B
+library ieee;
+use ieee.std_logic_1164.all;
 
-signal A: std_logic_vector(7 downto 0);
-A'left = 7
-A'right = 0
-A'low = 0
-A'high = 7
-A'range = 7 downto 0
-A'reverse_range = 0 to 7
-A'length = 8
-A'ascending = FALSE
+package My_Types is
+    type T is (A, B, C, D, E);
+    subtype S is T range D downto B;
+end package My_Types;
+
+entity Attribute_Example is
+    port (
+        input_A : in  std_logic_vector(7 downto 0);
+        input_B : in  std_logic_vector(7 downto 0);
+        result  : out std_logic_vector(15 downto 0)
+    );
+end Attribute_Example;
+
+architecture Behavioral of Attribute_Example is
+    use work.My_Types.all;
+    signal signal_A : std_logic_vector(7 downto 0);
+    signal signal_B : std_logic_vector(7 downto 0);
+    signal product  : std_logic_vector(15 downto 0);
+    signal my_enum  : S := D;  
+begin
+    process
+    begin
+        signal_A <= input_A;
+        signal_B <= input_B;
+        product <= std_logic_vector(unsigned(signal_A) * unsigned(signal_B));
+        product(product'left) <= '1'; 
+        wait;
+    end process;
+end Behavioral;
+
+
 
 --component declarations
-component mux4
-    port (sel, A, B, C, D: in  V2;
-          F              : out V2);
-end component;
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity component_instantiation is
+  port     (add1, add2: in integer;
+            sum_1: out integer);
+  end;
+  architecture example of component_instantiation is
+  component adder is
+  port     (a, b: in integer;
+            sum: out integer);
+  end component;
+  begin
+  component_inst: adder_component
+  port map (a => add1
+            b => add2,
+            sum => sum);
+end example;
+
 
 --Group declarations
 
@@ -112,7 +203,6 @@ architecture rtl of dsp is
       end if ;
     end process ;
   end block ;
-   
     ...
 end architecture rtl;
 
@@ -178,4 +268,88 @@ architecture wait_statement of adder is
 end wait_statement; --wait statement     this sensitivity list and wait statement are equivalent each other
 
 
+--Concurrent assertion statement
+library IEEE;
+use IEEE.IEEE.std_logic_1164.all;
+use IEEE._std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
+entity tb_address_check is
+end tb_address_check;
+
+architecture behavior of tb_address_check is
+    signal address : STD_LOGIC_VECTOR(31 downto 0);
+    component address_check
+        Port ( address : in STD_LOGIC_VECTOR(31 downto 0));
+    end component;
+begin
+    uut: address_check Port map (address => address);
+
+    stim_proc: process
+    begin
+        address <= x"0000_1234"; wait for 10 ns; 
+        address <= x"9000_1234"; wait for 10 ns; 
+        address <= x"4000_0000"; wait for 10 ns; 
+        address <= x"1000_0000"; wait for 10 ns; 
+        wait;
+    end process;
+    -- Concurrent assertion
+    assert (to_integer(unsigned(address)) < x"1000_0000" or to_integer(unsigned(address)) >= x"8000_0000")
+    report "Invalid access at address " & to_string(address)
+    severity warning;
+end behavior;
+
+
+--Concurrent signal assignment
+library IEEE;
+use IEEE.IEEE.std_logic_1164.all;
+
+entity add_1 is
+  port ( a, b, cin: in bit;
+         s, cout: out bit);
+ end add_1;
+ architecture concurrent of add_1 is
+  signal s1, s2, s3, s4: bit;
+ begin
+        s1 <= b xor cin;
+        s2 <= a and b;
+        s3 <= a and cin;
+        s4 <= b and cin;
+        s <= a xor s1;
+        cout <= s2 or s3 or s4;
+ end concurrent;
+
+--component instantiation
+--same example as component example
+
+--entity instantiation statements
+entity WORK.X (Y) port map (P1 => S1, P2 => S2);
+
+
+--generate statements
+entity full_adder1 is
+  Port (a        : in std_logic;
+        b        : in std_logic;
+        carry_in : in std_logic;
+        sum      : out std_logic;
+        carry_out: out std_logicr);
+end full_adder1;
+
+--full code
+library IEEE;
+use IEEE.IEEE.std_logic_1164.all;
+use IEEE._std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+architecture structural of adder is
+  signal carry_in_vector: std_logic_vector( 0 to WIDTH- 1);
+  signal carry_out_vector: std_logic_vector( 0 to WIDTH- 1);
+  begin
+    instantiate_adders: for i in 0 to WIDTH- 1 generate --generate statement and entity instantiation
+    adder_bit: entity work.full_adder1
+    port map (a = > a(i), b = > b(i), carry_in = > carry_in_vector(i),
+    sum = > sum(i), carry_out = > carry_out_vector(i));
+  end generate ;
+  carry_in_vector <= (carry_in, carry_out_vector( 0 to WIDTH- 2));
+  carry_out <= carry_out_vector(WIDTH- 1);
+ end structural;
