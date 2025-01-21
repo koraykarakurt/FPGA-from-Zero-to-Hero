7fe1e1ba77a3c6c804883ddbcbfc05c0cebe8405
mdemir04 <demir00mehmet@gmail.com>
2025-01-21 23:37:51 +0300

    FIRF-9: updated naming standard

--- user_folders/mehmet_d/hdl/comb_multiplier_mdl.vhd
+++ user_folders/mehmet_d/hdl/comb_multiplier_mdl.vhd
@@ -10,7 +10,7 @@
 -- Dependencies: NA
 -- Description: Combinational multiplier module with signed/unsigned and bit length generics.
 -- 
--- Revision --> 01v00; Date --> 20.01.25; JIRA No --> FIRF-9; Reason --> First Release
+-- Revision --> 01v00; Date --> 21.01.25; JIRA No --> FIRF-9; Reason --> First Release
 -- 
 --------------------------------------------------------------------------------------------------
 
@@ -20,13 +20,13 @@ use IEEE.numeric_std.all; -- for arithmetic operations
 
 entity comb_multiplier_mdl is
   generic (
-    selectsign_g : std_logic              := '0'; -- '0': unsigned, '1': signed
-    bitlength_g  : integer range 0 to 255 := 8 -- define bit length for both inputs as same, the output length will be sum of the input lengths
+    select_sign_g : std_logic              := '0'; -- '0': unsigned, '1': signed
+    bit_length_g  : integer range 0 to 255 := 8 -- define bit length for both inputs as same, the output length will be sum of the input lengths
   );
   port (
-    mult_1_i   : in std_logic_vector(bitlength_g - 1 downto 0);
-    mult_2_i   : in std_logic_vector(bitlength_g - 1 downto 0);
-    multrslt_o : out std_logic_vector((bitlength_g + bitlength_g) - 1 downto 0)
+    mult_1_i    : in std_logic_vector(bit_length_g - 1 downto 0);
+    mult_2_i    : in std_logic_vector(bit_length_g - 1 downto 0);
+    mult_rslt_o : out std_logic_vector((bit_length_g + bit_length_g) - 1 downto 0)
   );
 end comb_multiplier_mdl;
 
@@ -34,12 +34,12 @@ architecture behavioral_rtl of comb_multiplier_mdl is
 
 begin
 
-  unsigned_ifgnrt : if selectsign_g = '0' generate -- unsigned
-    multrslt_o <= std_logic_vector((unsigned(mult_1_i) * unsigned(mult_2_i)));
-  end generate unsigned_ifgnrt;
+  unsigned_if_gnrt : if select_sign_g = '0' generate -- unsigned
+    mult_rslt_o <= std_logic_vector((unsigned(mult_1_i) * unsigned(mult_2_i)));
+  end generate unsigned_if_gnrt;
 
-  signed_ifgnrt : if selectsign_g = '1' generate -- signed
-    multrslt_o <= std_logic_vector((signed(mult_1_i) * signed(mult_2_i)));
-  end generate signed_ifgnrt;
+  signed_if_gnrt : if select_sign_g = '1' generate -- signed
+    mult_rslt_o <= std_logic_vector((signed(mult_1_i) * signed(mult_2_i)));
+  end generate signed_if_gnrt;
 
 end behavioral_rtl;
