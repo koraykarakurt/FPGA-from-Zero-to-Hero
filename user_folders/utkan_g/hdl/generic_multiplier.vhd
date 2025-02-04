eeaa6ee579e26300e4564565916e6bb903630eac
utkang <utkangencdev@gmail.com>
2025-02-04 10:05:23 +0300

    GFIR-44: Add a description.

--- user_folders/utkan_g/hdl/generic_multiplier.vhd
+++ user_folders/utkan_g/hdl/generic_multiplier.vhd
@@ -1,12 +1,13 @@
 ---------------------------------------------------------------------------------------------------
 -- Author : Utkan Genc
 -- Description : 
---   
---   
+--   This is a RTL implementation of a generic multiplier.
 --   
 -- More information (optional) :
--- 
--- 
+--   MULTIPLIER_TYPE : 0 for unsigned, 1 for signed
+--   DATA_WIDTH : width of the inputs
+--   mult_in_1, mult_in_2 : inputs of the multiplier
+--   mult_out : output of the multiplier
 ---------------------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -14,8 +15,8 @@ use IEEE.numeric_std.all;
 
 entity generic_multiplier is
    generic (
-      MULTIPLIER_TYPE : integer range 0 to 1 := 0;  -- 0 for unsigned, 1 for signed
-      DATA_WIDTH      : integer range 1 to 64 := 8  -- width of the inputs
+      MULTIPLIER_TYPE : integer range 0 to 1 := 0;
+      DATA_WIDTH      : integer range 1 to 64 := 8
    );
    port (
       mult_in_1 : in  std_logic_vector(DATA_WIDTH-1 downto 0);
