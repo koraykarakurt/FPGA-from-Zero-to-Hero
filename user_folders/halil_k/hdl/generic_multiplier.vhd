e7cebf3854f0a65da4798964e938064168dae283
hfkimkak <furkankimkak@gmail.com>
2025-02-23 01:17:25 +0300

    FIRF-18: Add comments. Change the boolean name from SWITCHER to MULT_TYPE. Change the architecture type to if generate.

--- user_folders/halil_k/hdl/generic_multiplier.vhd
+++ user_folders/halil_k/hdl/generic_multiplier.vhd
@@ -14,24 +14,23 @@ use IEEE.numeric_std.all;
 
 entity generic_multiplier is
    Generic (    
-      SWITCHER : boolean               := true;
-      MULT_LEN : integer range 1 to 64 := 16
+      MULT_TYPE : boolean               := true; -- when it is true multiplication is signed else unsigned
+      MULT_LEN : integer range 1 to 64 := 16    
    );
    Port (    
-      mult_in_1 : in std_logic_vector(MULT_LEN-1 downto 0);      
-      mult_in_2 : in std_logic_vector(MULT_LEN-1 downto 0);     
-      mult_out  : out std_logic_vector((2*MULT_LEN)-1 downto 0)     
+      mult_in_1 : in std_logic_vector(MULT_LEN-1 downto 0); -- Input 1
+      mult_in_2 : in std_logic_vector(MULT_LEN-1 downto 0); -- Input 2
+      mult_out  : out std_logic_vector((2*MULT_LEN)-1 downto 0) -- Output
    );
 end generic_multiplier;
 
 architecture Behavioral of generic_multiplier is
-begin
-   process(mult_in_1, mult_in_2)
-   begin
-      if SWITCHER = true then
-          mult_out <= std_logic_vector(signed(mult_in_1) * signed(mult_in_2));
-      else
-          mult_out <= std_logic_vector(unsigned(mult_in_1) * unsigned(mult_in_2));
-      end if;
-   end process;
+begin   
+   SIGNED_MULT: if MULT_TYPE = true generate
+      mult_out <= std_logic_vector(signed(mult_in_1) * signed(mult_in_2));
+   end generate SIGNED_MULT;
+   
+   UNSIGNED_MULT: if MULT_TYPE = false generate
+      mult_out <= std_logic_vector(unsigned(mult_in_1) * unsigned(mult_in_2));
+   end generate UNSIGNED_MULT;
 end Behavioral;
