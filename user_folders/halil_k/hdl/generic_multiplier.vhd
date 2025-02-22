b2cd932ac03b6e03f49766674468131f384e536f
hfkimkak <furkankimkak@gmail.com>
2025-02-23 01:30:02 +0300

    FIRF-18: Fix the upper cases of behavioral and the name of generate blocks to lower cases.

--- user_folders/halil_k/hdl/generic_multiplier.vhd
+++ user_folders/halil_k/hdl/generic_multiplier.vhd
@@ -15,22 +15,22 @@ use IEEE.numeric_std.all;
 entity generic_multiplier is
    Generic (    
       MULT_TYPE : boolean               := true; -- when it is true multiplication is signed else unsigned
-      MULT_LEN : integer range 1 to 64 := 16    
+      MULT_LEN  : integer range 2 to 64  := 16    
    );
    Port (    
       mult_in_1 : in std_logic_vector(MULT_LEN-1 downto 0); -- Input 1
       mult_in_2 : in std_logic_vector(MULT_LEN-1 downto 0); -- Input 2
-      mult_out  : out std_logic_vector((2*MULT_LEN)-1 downto 0) -- Output
+      mult_out  : out std_logic_vector(2*MULT_LEN-1 downto 0) -- Output
    );
 end generic_multiplier;
 
-architecture Behavioral of generic_multiplier is
+architecture behavioral of generic_multiplier is
 begin   
-   SIGNED_MULT: if MULT_TYPE = true generate
+   signed_mult: if MULT_TYPE = true generate
       mult_out <= std_logic_vector(signed(mult_in_1) * signed(mult_in_2));
-   end generate SIGNED_MULT;
+   end generate signed_mult;
    
-   UNSIGNED_MULT: if MULT_TYPE = false generate
+   unsigned_mult: if MULT_TYPE = false generate
       mult_out <= std_logic_vector(unsigned(mult_in_1) * unsigned(mult_in_2));
-   end generate UNSIGNED_MULT;
-end Behavioral;
+   end generate unsigned_mult;
+end behavioral;
