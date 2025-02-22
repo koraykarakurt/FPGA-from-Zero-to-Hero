b40fd4a955351cfe51594f4fc74a085ac48dd02d
hfkimkak <furkankimkak@gmail.com>
2025-02-23 00:55:19 +0300

    FIRF-18: Change the file in order to make same with the entity title.

--- user_folders/halil_k/hdl/generic_multiplier.vhd
+++ user_folders/halil_k/hdl/generic_multiplier.vhd
@@ -12,18 +12,19 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity firf_1 is
+entity generic_multiplier is
    Generic (    
       SWITCHER        :       boolean                 := true;
       MULT_LEN        :       integer range 1 to 64   := 16
    );
    Port (    
-      mult_in_1       :       in      std_logic_vector(MULT_LEN downto 0);      
-      mult_in_2       :       in      std_logic_vector(MULT_LEN downto 0);     
-      mult_out        :       out     std_logic_vector(2*(MULT_LEN-1) downto 0)     
+      mult_in_1       :       in      std_logic_vector(MULT_LEN-1 downto 0);      
+      mult_in_2       :       in      std_logic_vector(MULT_LEN-1 downto 0);     
+      mult_out        :       out     std_logic_vector((2*MULT_LEN)-1 downto 0)     
    );
-end firf_1;
-architecture Behavioral of firf_1 is
+end generic_multiplier;
+
+architecture Behavioral of generic_multiplier is
 begin
    process(mult_in_1, mult_in_2)
    begin
