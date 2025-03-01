ab594ee9b9019f580e555e5e29ee54efc2c847fa
Koray Karakurt <43293347+koraykarakurt@users.noreply.github.com>
2025-03-01 10:12:06 +0100

    FIRF-40: Update ffzth_linting_rules.vhd

--- docs/linting_rules/ffzth_linting_rules.vhd
+++ docs/linting_rules/ffzth_linting_rules.vhd
@@ -14,16 +14,16 @@ use IEEE.numeric_std.all;
 
 entity ffzth_linting_rules is
    generic (
-      MULT_TYPE  : std_logic              := '0'; -- '0': unsigned, '1': signed
-      RESET_TYPE : std_logic              := '0'; -- '0': synchronous, '1': asynchronous (asynchronous option assumes that reset de-assertion is done outside of this block)
-      BIT_WIDTH  : natural range 2 to 256 :=  8   -- min value is 2, max value is 256
+      MULT_TYPE   : std_logic              := '0'; -- '0': unsigned, '1': signed
+      RESET_TYPE  : std_logic              := '0'; -- '0': synchronous, '1': asynchronous (asynchronous option assumes that reset de-assertion is done outside of this block)
+      BIT_WIDTH   : natural range 2 to 256 :=  8   -- min value is 2, max value is 256
    );
    port (
       clk_400mhz  : in  std_logic;
       rst_400mhz  : in  std_logic;                                 -- active high reset input in clk_400mhz domain
       mult_1      : in  std_logic_vector(BIT_WIDTH - 1 downto 0);  -- multiplier 1 in clk_400mhz domain
       mult_2      : in  std_logic_vector(BIT_WIDTH - 1 downto 0);  -- multiplier 2 in clk_400mhz domain
-      mult_result : out std_logic_vector((BIT_WIDTH + BIT_WIDTH) - 1 downto 0) := (others => '0')
+      mult_result : out std_logic_vector(2*BIT_WIDTH - 1 downto 0) := (others => '0')
    );
 end ffzth_linting_rules;
 
@@ -37,7 +37,7 @@ architecture behavioral of ffzth_linting_rules is
    signal my_state : ENUM_STATE := IDLE;
    signal error_counter : unsigned (ERROR_COUNTER_WIDTH - 1 downto 0) := (others => '0');
 begin
-   -- signed multiplication and synchronous reset
+   -- signed multiplication and synchronous reset section
    signed_synchronous_gen : if MULT_TYPE = '1' and RESET_TYPE = '0' generate 
       process(clk_400mhz)
       begin
@@ -50,7 +50,7 @@ begin
          end if;
       end process;
    end generate signed_synchronous_gen;
-   -- unsigned multiplication and synchronous reset
+   -- unsigned multiplication and synchronous reset section
    unsigned_synchronous_gen : if MULT_TYPE = '0' and RESET_TYPE = '0' generate  
       process(clk_400mhz)
       begin
@@ -63,7 +63,7 @@ begin
          end if;
       end process;
    end generate unsigned_synchronous_gen;
-   -- signed multiplication and asynchronous reset
+   -- signed multiplication and asynchronous reset section
    signed_asynchronous_gen : if MULT_TYPE = '1' and RESET_TYPE = '1' generate
       process(clk_400mhz,rst_400mhz)
       begin
@@ -74,7 +74,7 @@ begin
          end if;
       end process;
    end generate signed_asynchronous_gen;
-   -- unsigned multiplication and asynchronous reset
+   -- unsigned multiplication and asynchronous reset section
    unsigned_asynchronous_gen : if MULT_TYPE = '0' and RESET_TYPE = '1' generate  
       process(clk_400mhz,rst_400mhz)
       begin
