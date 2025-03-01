c4024b7f36d48a01145c890487501d989267e09f
mdemir04 <demir00mehmet@gmail.com>
2025-03-01 22:56:04 +0300

    FIRF-36: update according to review on the weekly meeting at 24.02.2025
    - generic_reset_bridge
    lower case the "std_logic_1164"
    renamed "intel" VENDOR as "altera"
    removed "shreg_extract" attribute for Xilinx
    added "dont_touch" attribute for Xilinx
    added "iob" attribute for Xilinx but not in generate part because error ic occurred... will be looked later

--- user_folders/mehmet_d/hdl/generic_reset_bridge.vhd
+++ user_folders/mehmet_d/hdl/generic_reset_bridge.vhd
@@ -10,32 +10,37 @@
 ---------------------------------------------------------------------------------------------------
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+use IEEE.std_logic_1164.all;
 
 entity generic_reset_bridge is
    generic (
-      VENDOR              : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "intel", "other"
+      VENDOR              : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera", "other"
       RESET_ACTIVE_STATUS : std_logic            := '1'; -- '0': active low, '1': active high
       SYNCH_FF_NUMBER     : natural range 2 to 5 := 3 --  adjust according to FPGA/SoC family, clock speed and input rate of change
    );
    port (
-      clk     : in std_logic;
+      clk     : in std_logic; -- module clock
       rst_in  : in std_logic; -- asynchronous reset input
-      rst_out : out std_logic -- asynchronously asserted synchronously de-asserted output
+      rst_out : out std_logic -- asynchronously asserted synchronously de-asserted reset output
    );
 end generic_reset_bridge;
 
 architecture behavioral of generic_reset_bridge is
+
+   attribute iob           : string;
+   attribute iob of rst_in : signal is "true";
 
 begin
 
    xilinx_gen : if VENDOR = "xilinx" generate
       signal rst_reg : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => (not RESET_ACTIVE_STATUS));
 
-      attribute async_reg                : string;
-      attribute async_reg of rst_reg     : signal is "true";
-      attribute shreg_extract            : string;
-      attribute shreg_extract of rst_reg : signal is "no";
+      attribute async_reg             : string;
+      attribute async_reg of rst_reg  : signal is "true";
+      attribute dont_touch            : string;
+      attribute dont_touch of rst_reg : signal is "true";
+      --attribute iob                   : string;
+      --attribute iob of rst_in         : signal is "true";
    begin
       xilinx : process (clk, rst_in)
       begin
@@ -50,16 +55,17 @@ begin
       rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out      
    end generate xilinx_gen;
 
-   intel_gen : if VENDOR = "intel" generate
+   intel_gen : if VENDOR = "altera" generate
       signal rst_reg : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => (not RESET_ACTIVE_STATUS));
 
-      attribute altera_attribute of data_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
-      attribute dont_merge                   : boolean;
-      attribute dont_merge of data_reg       : signal is true;
-      attribute preserve                     : boolean;
-      attribute preserve of data_reg         : signal is true;
+      attribute altera_attribute            : string;
+      attribute altera_attribute of rst_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+      attribute dont_merge                  : boolean;
+      attribute dont_merge of rst_reg       : signal is true;
+      attribute preserve                    : boolean;
+      attribute preserve of rst_reg         : signal is true;
    begin
-      intel : process (clk, rst_in)
+      altera : process (clk, rst_in)
       begin
          if rst_in = RESET_ACTIVE_STATUS then
             rst_reg <= (others => RESET_ACTIVE_STATUS);
@@ -68,7 +74,7 @@ begin
             rst_reg <= rst_reg(SYNCH_FF_NUMBER - 2 downto 0) & (not RESET_ACTIVE_STATUS); -- shift left
 
          end if;
-      end process intel;
+      end process altera;
       rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out
    end generate intel_gen;
 
