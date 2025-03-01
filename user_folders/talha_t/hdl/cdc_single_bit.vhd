010a83a1e89e8f907e55a804551adf89cb407825
Koray Karakurt <mkoraykarakurt@hotmail.com>
2025-03-01 21:28:51 +0100

    GFIR-64: Online review updates

--- user_folders/talha_t/hdl/cdc_single_bit.vhd
+++ user_folders/talha_t/hdl/cdc_single_bit.vhd
@@ -16,57 +16,51 @@
 -- Additional Comments:
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_1164.all;
 
-entity generic_reset_bridge is
-	generic(
-		FF_SIZE_SYNC: natural range 2 to 6 := 3;
-		RST_ACTIVE_STATUS: std_logic := '1';
-		VENDOR : string := "efinix"
-	);
-    Port ( 
-		rst_in : in std_logic;
-		clk : in std_logic;
-		data_in : in std_logic;
-		data_out : out std_logic
-	);
-end generic_reset_bridge;
+entity generic_singlebit_cdc is
+   generic(
+      VENDOR : string := "efinix";
+      SYNCH_FF_NUMBER: natural range 2 to 6 := 3
+   );
+    port ( 
+      clk      : in std_logic;
+      rst      : in std_logic;
+      data_in  : in std_logic;
+      data_out : out std_logic
+   );
+end generic_singlebit_cdc;
 
-architecture behavioral of generic_reset_bridge is
-	signal data_sync_buffer : std_logic_vector (ff_size_sync-1 downto 0) := (others => (not RST_ACTIVE_STATUS));
-
+architecture behavioral of generic_singlebit_cdc is
+   signal data_sync_buffer : std_logic_vector (SYNCH_FF_NUMBER-1 downto 0) := (others => '0');
 begin
+-- Xilinx attributes IOB, ASYNC_REG ve DONT_TOUCH
+-- Altera attributes USEIOOFF, PRESERVE, ALTERA_ATTRIBUTE (inline constraint)
+   xilinx_version: if (VENDOR = "xilinx") generate
+      attribute async_reg : string;
+      attribute async_reg of data_sync_buffer : signal is "true";
+      attribute shreg_extract : string; --???? be sure 
+      attribute shreg_extract of data_sync_buffer : signal is "no"; --???? be sure 
+      xilinx_rst_sync : process(clk)
+      begin
+         if (rising_edge(clk)) then
+            data_sync_buffer <= data_sync_buffer(SYNCH_FF_NUMBER-2 downto 0) & data_in;
+         end if;
+      end process xilinx_rst_sync;
+   end generate xilinx_version;
+   
+   efinix_version : if (VENDOR = "efinix") generate
+      attribute async_reg: boolean;
+      attribute async_reg of data_sync_buffer : signal is true;
 
-	xilinx_version: if (VENDOR = "xilinx") generate
-		attribute async_reg : string;
-		attribute async_reg of data_sync_buffer : signal is "true";
-		attribute shreg_extract : string;
-		attribute shreg_extract of data_sync_buffer : signal is "no";
+      efinix_rst_sync : process(clk)
+      begin
+         if (rising_edge(clk)) then
+            data_sync_buffer <= data_sync_buffer(SYNCH_FF_NUMBER-2 downto 0) & data_in;
+         end if;
+      end process efinix_rst_sync;
+   end generate efinix_version;
 
-		xilinx_rst_sync : process(clk, rst_in)
-		begin
-			if (rst_in = RST_ACTIVE_STATUS) then
-				data_sync_buffer <= (others => RST_ACTIVE_STATUS);
-			elsif (rising_edge(clk)) then
-				data_sync_buffer <= data_sync_buffer(FF_SIZE_SYNC-2 downto 0) & (not RST_ACTIVE_STATUS);
-			end if;
-		end process xilinx_rst_sync;
-	end generate xilinx_version;
-	
-	efinix_version : if (VENDOR = "efinix") generate
-		attribute async_reg: boolean;
-		attribute async_reg of data_sync_buffer : signal is true;
-
-		efinix_rst_sync : process(clk, rst_in)
-		begin
-			if (rst_in = RST_ACTIVE_STATUS) then
-				data_sync_buffer <= (others => RST_ACTIVE_STATUS);
-			elsif (rising_edge(clk)) then
-				data_sync_buffer <= data_sync_buffer(FF_SIZE_SYNC-2 downto 0) & (not RST_ACTIVE_STATUS);
-			end if;
-		end process efinix_rst_sync;
-	end generate efinix_version;
-
-	rst_out <= data_sync_buffer(FF_SIZE_SYNC-1);
+   data_out <= data_sync_buffer(SYNCH_FF_NUMBER-1);
 
 end Behavioral;
