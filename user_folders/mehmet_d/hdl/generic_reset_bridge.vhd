30815bc8585477f725edcdf98e58fe7649207b97
mdemir04 <demir00mehmet@gmail.com>
2025-03-06 06:48:40 +0300

    FIRF-36: update according to review on the weekly meeting at 03.03.2025
    remove "other" part of "VENDOR" generic
    remove "iob" attribure of Xilinx
    uppercase the attibutes according to linting rules
    add process comment to the above of each process

--- user_folders/mehmet_d/hdl/generic_reset_bridge.vhd
+++ user_folders/mehmet_d/hdl/generic_reset_bridge.vhd
@@ -14,7 +14,7 @@ use IEEE.std_logic_1164.all;
 
 entity generic_reset_bridge is
    generic (
-      VENDOR              : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera", "other"
+      VENDOR              : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera"
       RESET_ACTIVE_STATUS : std_logic            := '1';      -- '0': active low, '1': active high
       SYNCH_FF_NUMBER     : natural range 2 to 5 := 3         --  adjust according to FPGA/SoC family, clock speed and input rate of change
    );
@@ -27,21 +27,18 @@ end generic_reset_bridge;
 
 architecture behavioral of generic_reset_bridge is
    -- xilinx attribute definitions
-   attribute async_reg  : string;
-   attribute dont_touch : string;
-   attribute iob        : string;
-   -- xilinx iob attribute is here since iob attribute requires entity port
-   attribute iob of rst_in : signal is "true";
+   attribute ASYNC_REG  : string;
+   attribute DONT_TOUCH : string;
    -- altera attribute definitions
-   attribute altera_attribute : string;
-   attribute preserve         : boolean;
-   attribute dont_merge       : boolean;
+   attribute ALTERA_ATTRIBUTE : string;
+   attribute DONT_MERGE       : boolean;
+   attribute PRESERVE         : boolean;
 begin
-
+   -- xilinx process
    xilinx_gen : if VENDOR = "xilinx" generate
       signal rst_reg                  : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => RESET_ACTIVE_STATUS);
-      attribute async_reg of rst_reg  : signal is "true";
-      attribute dont_touch of rst_reg : signal is "true";
+      attribute ASYNC_REG of rst_reg  : signal is "true";
+      attribute DONT_TOUCH of rst_reg : signal is "true";
    begin
       xilinx : process (clk, rst_in)
       begin
@@ -54,11 +51,12 @@ begin
       rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out      
    end generate xilinx_gen;
 
+   -- altera process
    altera_gen : if VENDOR = "altera" generate
       signal rst_reg                        : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => RESET_ACTIVE_STATUS);
-      attribute altera_attribute of rst_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
-      attribute dont_merge of rst_reg       : signal is true;
-      attribute preserve of rst_reg         : signal is true;
+      attribute ALTERA_ATTRIBUTE of rst_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+      attribute DONT_MERGE of rst_reg       : signal is true;
+      attribute PRESERVE of rst_reg         : signal is true;
    begin
       altera : process (clk, rst_in)
       begin
@@ -70,19 +68,5 @@ begin
       end process altera;
       rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out
    end generate altera_gen;
-
-   other_gen : if VENDOR = "other" generate
-      signal rst_reg : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => RESET_ACTIVE_STATUS);
-   begin
-      other_vendors : process (clk, rst_in)
-      begin
-         if rst_in = RESET_ACTIVE_STATUS then
-            rst_reg <= (others => RESET_ACTIVE_STATUS);
-         elsif rising_edge(clk) then
-            rst_reg <= rst_reg(SYNCH_FF_NUMBER - 2 downto 0) & (not RESET_ACTIVE_STATUS); -- shift left
-         end if;
-      end process other_vendors;
-      rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out
-   end generate other_gen;
 
 end behavioral;
