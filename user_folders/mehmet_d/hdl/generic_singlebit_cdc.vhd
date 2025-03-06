c674deaa993788a6e4365bf1c4bfe1e9d0c7db6e
mdemir04 <demir00mehmet@gmail.com>
2025-03-06 06:49:08 +0300

    FIRF-36: update according to review on the weekly meeting at 03.03.2025
    remove "iob" attribure of Xilinx
    remove initialization of "data_reg" signal
    uppercase the attibutes according to linting rules
    add process comment to the above of each process

--- user_folders/mehmet_d/hdl/generic_singlebit_cdc.vhd
+++ user_folders/mehmet_d/hdl/generic_singlebit_cdc.vhd
@@ -14,7 +14,7 @@ use IEEE.std_logic_1164.all;
 
 entity generic_singlebit_cdc is
    generic (
-      VENDOR          : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera", "other"
+      VENDOR          : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera"
       SYNCH_FF_NUMBER : natural range 2 to 5 := 3         --  adjust according to FPGA/SoC family, clock speed and input rate of change
    );
    port (
@@ -26,18 +26,18 @@ end generic_singlebit_cdc;
 
 architecture behavioral of generic_singlebit_cdc is
    -- xilinx attribute definitions
-   attribute async_reg  : string;
-   attribute dont_touch : string;
+   attribute ASYNC_REG  : string;
+   attribute DONT_TOUCH : string;
    -- altera attribute definitions
-   attribute altera_attribute : string;
-   attribute dont_merge       : boolean;
-   attribute preserve         : boolean;
+   attribute ALTERA_ATTRIBUTE : string;
+   attribute DONT_MERGE       : boolean;
+   attribute PRESERVE         : boolean;
 begin
-
+   -- xilinx process
    xilinx_gen : if VENDOR = "xilinx" generate
-      signal data_reg                   : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => '0');
-      attribute async_reg of data_reg   : signal is "true";
-      attribute dont_touch of rst_chain : signal is "true";
+      signal data_reg                   : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0);
+      attribute ASYNC_REG of data_reg   : signal is "true";
+      attribute DONT_TOUCH of rst_chain : signal is "true";
    begin
       xilinx : process (clk)
       begin
@@ -48,11 +48,12 @@ begin
       data_out <= data_reg(SYNCH_FF_NUMBER - 1); -- assing MSb data to out
    end generate xilinx_gen;
 
+   -- altera process
    altera_gen : if VENDOR = "altera" generate
-      signal data_reg                        : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => '0');
-      attribute altera_attribute of data_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
-      attribute dont_merge of data_reg       : signal is true;
-      attribute preserve of data_reg         : signal is true;
+      signal data_reg                        : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0);
+      attribute ALTERA_ATTRIBUTE of data_reg : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+      attribute DONT_MERGE of data_reg       : signal is true;
+      attribute PRESERVE of data_reg         : signal is true;
    begin
       altera : process (clk)
       begin
@@ -62,17 +63,5 @@ begin
       end process altera;
       data_out <= data_reg(SYNCH_FF_NUMBER - 1); -- assing MSb data to out
    end generate altera_gen;
-
-   other_gen : if VENDOR = "other" generate
-      signal data_reg : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => '0');
-   begin
-      other_vendors : process (clk)
-      begin
-         if rising_edge(clk) then
-            data_reg <= data_reg(SYNCH_FF_NUMBER - 2 downto 0) & data_in; -- shift left
-         end if;
-      end process other_vendors;
-      data_out <= data_reg(SYNCH_FF_NUMBER - 1); -- assing MSb data to out
-   end generate other_gen;
 
 end behavioral;
