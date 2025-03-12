8912b7b952f256d0d40ad1ea0da0374ec55ad21c
mdemir04 <demir00mehmet@gmail.com>
2025-03-12 07:18:25 +0300

    FIRF-36: add microchip attributes

--- user_folders/mehmet_d/hdl/generic_reset_bridge.vhd
+++ user_folders/mehmet_d/hdl/generic_reset_bridge.vhd
@@ -1,8 +1,8 @@
 ---------------------------------------------------------------------------------------------------
 -- Author : Mehmet DEMİR
--- Description : Asynchronous reset synchronizer chain with generics.
---   
---   
+-- Description : Asynchronous reset synchronizer chain with generics. This module used in
+-- asynchronous resets to prevent metastability issues. Remember that "rst_out" is asynchronously
+-- asserted and synchronously de-asserted. 
 --   
 -- More information (optional) :
 --    
@@ -14,7 +14,7 @@ use IEEE.std_logic_1164.all;
 
 entity generic_reset_bridge is
    generic (
-      VENDOR              : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera", "efinity", "lattice"
+      VENDOR              : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera", "efinity", "microchip", "lattice"
       RESET_ACTIVE_STATUS : std_logic            := '1';      -- '0': active low, '1': active high
       SYNCH_FF_NUMBER     : natural range 2 to 5 := 3         --  adjust according to FPGA/SoC family, clock speed and input rate of change
    );
@@ -68,6 +68,26 @@ begin
       end process altera;
       rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out
    end generate altera_gen;
+
+   -- microchip process
+   microchip_gen : if VENDOR = "microchip" generate
+      signal rst_reg : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => RESET_ACTIVE_STATUS);
+      -- microchip attributes
+      attribute SYN_KEEP                : boolean;
+      attribute SYN_KEEP of rst_reg     : signal is true;
+      attribute SYN_PRESERVE            : boolean;
+      attribute SYN_PRESERVE of rst_reg : signal is true;
+   begin
+      microchip : process (clk, rst_in)
+      begin
+         if rst_in = RESET_ACTIVE_STATUS then
+            rst_reg <= (others => RESET_ACTIVE_STATUS);
+         elsif rising_edge(clk) then
+            rst_reg <= rst_reg(SYNCH_FF_NUMBER - 2 downto 0) & (not RESET_ACTIVE_STATUS); -- shift left
+         end if;
+      end process microchip;
+      rst_out <= rst_reg(SYNCH_FF_NUMBER - 1); -- assing MSb to out
+   end generate microchip_gen;
 
    -- efinity process
    efinity_gen : if VENDOR = "efinity" generate
