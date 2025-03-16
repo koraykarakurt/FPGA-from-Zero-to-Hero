4ea6bb0802d54fedd9d998c43d0aff57f84ff05a
dagyunus <dagyns@outlook.com>
2025-03-16 22:43:32 +0300

    FIRF-37 Add timing constraints for Xilinx

---
+++ user_folders/yunus_d/hdl/generic_reset_bridge.vhd
@@ -0,0 +1,65 @@
+---------------------------------------------------------------------------------------------------
+-- Author      : Yunus Dag
+-- Description : generic_singlebit_cdc
+--   
+--   
+--   
+-- More information (optional) : When reset is active, all flip-flops are reset, 
+-- and the reset signal reaches the output by bridging with the each clock pulse.   
+--    
+---------------------------------------------------------------------------------------------------
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity generic_reset_bridge is
+generic (
+VENDOR 					: string              := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "intel", “other”
+RESET_ACTIVE_STATUS : std_logic            := '1'; -- '0': active low, '1': active high
+SYNCH_FF_NUMBER     : natural range 2 to 5 := 3 -- adjust according to FPGA/SoC family, clock speed and input rate of change
+);
+port(
+	clk 		: in std_logic;
+	rst_in 	: in std_logic;
+	rst_out	: out std_logic
+);
+end generic_reset_bridge;
+
+architecture behavioral of generic_reset_bridge is
+begin
+
+XILINX_GEN : if VENDOR = "xilinx" generate
+signal sync_regs : std_logic_vector(SYNCH_FF_NUMBER -1 downto 0) := (others => ('0')); 
+attribute ASYNC_REG : string;
+attribute ASYNC_REG of sync_regs : signal is "true";
+attribute DONT_TOUCH : string;
+attribute DONT_TOUCH of sync_regs : signal is "true";
+begin
+	process(clk, rst_in) begin
+		if(rst_in=RESET_ACTIVE_STATUS) then
+		sync_regs <= (others => (RESET_ACTIVE_STATUS));
+		elsif(rising_Edge(clk)) then
+		sync_regs <= sync_regs(SYNCH_FF_NUMBER -2 downto 0) & (not RESET_ACTIVE_STATUS);
+		end if;
+	end process;
+	rst_out <= sync_regs(SYNCH_FF_NUMBER -1 );
+end generate XILINX_GEN;
+
+ALTERA_GEN : if VENDOR = "altera" generate
+signal sync_regs : std_logic_vector(SYNCH_FF_NUMBER -1 downto 0) := (others => ('0')); 
+attribute PRESERVE : boolean;
+attribute PRESERVE of sync_regs : signal is true;
+attribute ALTERA_ATTRIBUTE : string;
+attribute ALTERA_ATTRIBUTE of sync_regs : signal is "-name SYNCHRONIZER_IDENTIFICATION FORCED";
+begin
+	process(clk, rst_in) begin
+		if(rst_in=RESET_ACTIVE_STATUS) then
+		sync_regs <= (others => (RESET_ACTIVE_STATUS));
+		elsif(rising_Edge(clk)) then
+		sync_regs <= sync_regs(SYNCH_FF_NUMBER -2 downto 0) & (not RESET_ACTIVE_STATUS);
+		end if;
+	end process;
+	rst_out <= sync_regs(SYNCH_FF_NUMBER -1 );
+end generate ALTERA_GEN;
+
+end behavioral;
---
+++ user_folders/yunus_d/hdl/generic_singlebit_cdc.vhd
@@ -0,0 +1,62 @@
+---------------------------------------------------------------------------------------------------
+-- Author      : Yunus Dag
+-- Description : generic_singlebit_cdc
+--   
+--   
+--   
+-- More information (optional) : This design sychonizes input signal data_in in clk clock domain
+-- and output is sychonized data_in signal    
+--    
+---------------------------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity generic_singlebit_cdc is
+generic (
+   VENDOR          : string               := "xilinx"; -- FPGA vendor name, valid values --> "xilinx", "altera"
+   SYNCH_FF_NUMBER : natural range 2 to 5 := 3 -- adjust according to FPGA/SoC family, clock speed and input rate of change
+);
+port(
+   clk      : in std_logic;
+   data_in  : in std_logic;
+   data_out : out std_logic
+);
+end generic_singlebit_cdc;
+
+architecture behavioral of generic_singlebit_cdc is
+begin
+
+XILINX_GEN : if VENDOR = "xilinx" generate
+signal sync_regs : std_logic_vector(SYNCH_FF_NUMBER -1 downto 0) := (others => ('0')); 
+attribute ASYNC_REG : string;
+attribute ASYNC_REG of sync_regs : signal is "true";
+attribute DONT_TOUCH : string;
+attribute DONT_TOUCH of sync_regs : signal is "true";
+begin
+process(clk)
+   begin
+      if(rising_Edge(clk)) then
+         sync_regs <= sync_regs(SYNCH_FF_NUMBER-2 downto 0) & data_in;
+      end if;
+   end process;
+   data_out <= sync_regs(SYNCH_FF_NUMBER-1);
+end generate XILINX_GEN;
+
+ALTERA_GEN : if VENDOR = "altera" generate
+signal sync_regs : std_logic_vector(SYNCH_FF_NUMBER -1 downto 0) := (others => ('0')); 
+attribute PRESERVE : boolean;
+attribute PRESERVE of sync_regs : signal is true;
+attribute ALTERA_ATTRIBUTE : string;
+attribute ALTERA_ATTRIBUTE of sync_regs : signal is "-name SYNCHRONIZER_IDENTIFICATION FORCED";
+begin
+process(clk) 
+   begin
+      if(rising_Edge(clk)) then
+         sync_regs <= sync_regs(SYNCH_FF_NUMBER-2 downto 0) & data_in;
+      end if;
+   end process;
+   data_out <= sync_regs(SYNCH_FF_NUMBER-1);
+end generate ALTERA_GEN;
+end behavioral;
+
