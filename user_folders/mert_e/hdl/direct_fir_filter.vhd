37c9e5700e9167b7714fa488d63e9f3b74c5b033
MertE <mertecevit@protonmail.com>
2025-03-23 18:03:57 +0300

    GFIR-5 Code update

--- user_folders/mert_e/hdl/direct_fir_filter.vhd
+++ user_folders/mert_e/hdl/direct_fir_filter.vhd
@@ -17,13 +17,13 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use work.fir_filter_coeffs.all;
+use work.fir_filter_coeff.all;
 
 
 
 entity direct_fir_filter is
    generic (
-      number_of_taps        : integer := 4; 
+      number_of_taps        : integer := 16; 
       input_width           : natural := 18;
       coeff_width           : natural := 18;
       output_width          : natural := 54 -- guard_bits = 54 - 18 - 18 = 18
@@ -33,11 +33,12 @@ entity direct_fir_filter is
       rst                   : in  std_logic;
       valid_in              : in  std_logic;
       data_in               : in  std_logic_vector(input_width-1 downto 0);
+      coeff_pipe            : in pipeline_coeffs;
       data_out              : out std_logic_vector(output_width-1 downto 0);
       valid_out             : out std_logic
    );
    attribute syn_dspstyle: string;
-   attribute syn_dspstyle of result: signal is "logic";
+   attribute syn_dspstyle of data_out: signal is "logic";
 end direct_fir_filter;
 
 architecture behavioral of direct_fir_filter is
@@ -51,7 +52,7 @@ architecture behavioral of direct_fir_filter is
    signal valid_pipe        : std_logic_vector(number_of_taps-1 downto 0) := (others => '0');
 
    -- Use Coefficients from fir_filter_coeffs
-   signal coeff_pipe : pipeline_coeffs := coeff_pipe;
+  -- signal coeff_pipe : pipeline_coeffs := coeff_pipe;
 
    type pipeline_products is array (0 to number_of_taps-1) of signed(input_width+coeff_width-1 downto 0);
    signal product_pipe      : pipeline_products := (others => (others => '0'));
@@ -78,11 +79,12 @@ begin
                 data_pipe(0) <= signed(data_in);
                 -- Performing multiplications
                 for idx in 0 to number_of_taps-1 loop
-                    product_pipe(idx) <= data_pipe(idx) * coeff_pipe(idx);
+                    
                     if idx = 0 then
                         sum_pipe(idx) <= resize(product_pipe(idx), output_width);
+                        
                     else
-                        sum_pipe(idx) <= sum_pipe(idx-1) + resize(product_pipe(idx), output_width);
+                        sum_pipe(idx) <= sum_pipe(idx-1) + resize(data_pipe(idx) * coeff_pipe(idx), output_width);
                     end if;
                 end loop;
                 -- Valid pipeline shifting
--- user_folders/mert_e/hdl/fir_filter_coeff.vhd
+++ user_folders/mert_e/hdl/fir_filter_coeff.vhd
@@ -5,18 +5,30 @@ use IEEE.numeric_std.all;
 package fir_filter_coeff is
 
   -- Generic Parameters
-  constant number_of_taps : integer := 4;    -- Number of filter taps
+  constant number_of_taps : integer := 16;    -- Number of filter taps
   constant coeff_width    : integer := 18;   -- Coefficient width (including sign)
 
   -- Coefficient Type
   type pipeline_coeffs is array (0 to number_of_taps-1) of signed(coeff_width-1 downto 0);
   
   -- Coefficient Values
-  constant coeff_pipe : pipeline_coeffs := (
-    "00" & x"0001", 
-    "00" & x"0002", 
-    "00" & x"0003", 
-    "00" & x"0004"
-  );
+--  constant coeff_pipe : pipeline_coeffs := (
+--    "00" & x"0001", 
+--    "00" & x"0002", 
+--    "00" & x"0003", 
+--    "00" & x"0004",
+--    "00" & x"0005",
+--    "00" & x"0006",
+--    "00" & x"0007",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003",
+--    "00" & x"0003"
+--  );
 
 end fir_filter_coeff;
