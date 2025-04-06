72d11d2538911ae2cb7d74099e5ede2bbd89ad05
Recepbuyuktuncer <94556340+recepbuyuktuncer@users.noreply.github.com>
2025-04-06 17:18:19 +0300

    Update generic_singlebit_cdc.vhd
    The requested changes have been attempted.

--- user_folders/recep_b/hdl/generic_singlebit_cdc.vhd
+++ user_folders/recep_b/hdl/generic_singlebit_cdc.vhd
@@ -9,36 +9,57 @@
 --    
 ---------------------------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity generic_singlebit_cdc is
     generic (
-        VENDOR : string := "Xilinx";               -- Vendor specification 
-        SYNCH_FF_NUMBER : integer range 2 to 5 := 2 -- Number of flip-flops for synchronization
+        VENDOR          : string               := "xilinx"; 
+        SYNCH_FF_NUMBER : natural range 2 to 5 := 3
     );
     port (
-        clk      : in  std_logic;  -- Single clock domain
-        data_in  : in  std_logic;  -- Single-bit input signal
-        data_out : out std_logic   
+        clk      : in std_logic;
+        data_in  : in std_logic;
+        data_out : out std_logic
     );
-end entity;
+end generic_singlebit_cdc;
 
 architecture behavioral of generic_singlebit_cdc is
-   signal sync_chain : std_logic_vector(SYNCH_FF_NUMBER-1 downto 0) := (others => '0');
-   attribute ASYNC_REG : string;
-   attribute ASYNC_REG of sync_chain : signal is "TRUE";
-   attribute DONT_TOUCH : string;
-   attribute DONT_TOUCH of sync_chain : signal is "TRUE";
 begin
-    -- Synchronization process
-    sync_process : process (clk)
-   begin
-       if rising_edge(clk) then
-           -- Shift the synchronization chain
-           sync_chain <= sync_chain(SYNCH_FF_NUMBER-2 downto 0) & data_in;
-       end if;
-   end process sync_process;
 
-   -- Output the synchronized signal
-   data_out <= sync_chain(SYNCH_FF_NUMBER-1);
+    xilinx_section : if VENDOR = "xilinx" generate
+        signal regs : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => '0');
+        attribute DONT_TOUCH : string;
+        attribute ASYNC_REG : string;
+        attribute DONT_TOUCH of regs : signal is "true";
+        attribute ASYNC_REG of regs : signal is "true";
+    begin
+        process(clk)
+        begin
+            if rising_edge(clk) then
+                regs <= regs(SYNCH_FF_NUMBER - 2 downto 0) & data_in;
+            end if;
+        end process;
+        data_out <= regs(SYNCH_FF_NUMBER - 1);
+    end generate;
+
+    altera_section : if VENDOR = "altera" generate
+        signal regs : std_logic_vector(SYNCH_FF_NUMBER - 1 downto 0) := (others => '0');
+        attribute PRESERVE : boolean;
+        attribute ALTERA_ATTRIBUTE : string;
+        attribute PRESERVE of regs : signal is true;
+        attribute ALTERA_ATTRIBUTE of regs : signal is 
+            "-name SDC_STATEMENT ""set_false_path -from [get_pins data_in]"";" &
+            "-name SYNCHRONIZER_IDENTIFICATION ""FORCED"";" &
+            "-name SYNCHRONIZATION_REGISTER_CHAIN_LENGTH " & integer'image(SYNCH_FF_NUMBER - 1);
+    begin
+        process(clk)
+        begin
+            if rising_edge(clk) then
+                regs <= regs(SYNCH_FF_NUMBER - 2 downto 0) & data_in;
+            end if;
+        end process;
+        data_out <= regs(SYNCH_FF_NUMBER - 1);
+    end generate;
+
 end behavioral;
