ee864dabd17f9251127a16add20bae69e87787aa
Recepbuyuktuncer <94556340+recepbuyuktuncer@users.noreply.github.com>
2025-04-06 17:14:37 +0300

    Update generic_reset_bridge.vhd
    The requested changes have been attempted.

--- user_folders/recep_b/hdl/generic_reset_bridge.vhd
+++ user_folders/recep_b/hdl/generic_reset_bridge.vhd
@@ -10,34 +10,64 @@
 ---------------------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity generic_reset_bridge is
-   generic (
-        VENDOR : string := "Xilinx";               -- Vendor specification 
-        RESET_ACTIVE_STATUS : std_logic := '1';   -- Active high or low reset 
-        SYNCH_FF_NUMBER : integer range 2 to 5 := 2 -- Number of flip-flops for synchronization
+    generic (
+        VENDOR              : string := "xilinx";              -- "xilinx", "altera", or "other"
+        RESET_ACTIVE_STATUS : std_logic := '1';                -- '1': active high, '0': active low
+        SYNCH_FF_NUMBER     : natural range 2 to 5 := 3        -- Flip-flop stages for synchronization
     );
-   port (
-        clk     : in  std_logic;  -- Clock input
-        rst_in  : in  std_logic;  -- Asynchronous reset input
-        rst_out : out std_logic   
+    port (
+        clk     : in  std_logic;       -- Clock input
+        rst_in  : in  std_logic;       -- Asynchronous reset input
+        rst_out : out std_logic        -- Synchronized reset output
     );
 end entity;
 
 architecture behavioral of generic_reset_bridge is
-    -- Internal signal for synchronization chain
-   signal rst_sync : std_logic_vector(SYNCH_FF_NUMBER-1 downto 0) := (others => (not RESET_ACTIVE_STATUS));
 begin
-   process (clk, rst_in)
-   begin
-        if rst_in = RESET_ACTIVE_STATUS then  -- Asynchronous reset condition
-            rst_sync <= (others => RESET_ACTIVE_STATUS);  -- Set all flip-flops to reset state
-        elsif rising_edge(clk) then           -- Synchronous operation on clock edge
-            -- Shift the synchronization chain
-            rst_sync <= rst_sync(SYNCH_FF_NUMBER-2 downto 0) & (not RESET_ACTIVE_STATUS);
-        end if;
-   end process;
 
-    -- Output the last flip-flop in the synchronization chain
-    rst_out <= rst_sync(SYNCH_FF_NUMBER-1);
-end behavioral;
+    --  XILINX VENDOR SYNC --
+    XILINX_GEN : if VENDOR = "xilinx" generate
+        signal sync_regs : std_logic_vector(SYNCH_FF_NUMBER -1 downto 0) := (others => '0');
+        attribute ASYNC_REG   : string;
+        attribute DONT_TOUCH  : string;
+        attribute ASYNC_REG of sync_regs   : signal is "true";
+        attribute DONT_TOUCH of sync_regs  : signal is "true";
+    begin
+        process(clk, rst_in)
+        begin
+            if rst_in = RESET_ACTIVE_STATUS then
+                sync_regs <= (others => RESET_ACTIVE_STATUS);
+            elsif rising_edge(clk) then
+                sync_regs <= sync_regs(SYNCH_FF_NUMBER - 2 downto 0) & (not RESET_ACTIVE_STATUS);
+            end if;
+        end process;
+        rst_out <= sync_regs(SYNCH_FF_NUMBER - 1);
+    end generate;
+
+    -- ALTERA (INTEL) VENDOR SYNC --
+    ALTERA_GEN : if VENDOR = "altera" generate
+        signal sync_regs : std_logic_vector(SYNCH_FF_NUMBER -1 downto 0) := (others => '0');
+        attribute PRESERVE         : boolean;
+        attribute ALTERA_ATTRIBUTE : string;
+        attribute PRESERVE of sync_regs : signal is true;
+        attribute ALTERA_ATTRIBUTE of sync_regs : signal is 
+            "-name SYNCHRONIZER_IDENTIFICATION ""FORCED""; " &
+            "-name SDC_STATEMENT ""set_false_path -from [get_ports rst_in]""; " &
+            "-name SYNCHRONIZATION_REGISTER_CHAIN_LENGTH " & integer'image(SYNCH_FF_NUMBER - 1);
+    begin
+        process(clk, rst_in)
+        begin
+            if rst_in = RESET_ACTIVE_STATUS then
+                sync_regs <= (others => RESET_ACTIVE_STATUS);
+            elsif rising_edge(clk) then
+                sync_regs <= sync_regs(SYNCH_FF_NUMBER - 2 downto 0) & (not RESET_ACTIVE_STATUS);
+            end if;
+        end process;
+        rst_out <= sync_regs(SYNCH_FF_NUMBER - 1);
+    end generate;
+
+end architecture;
+
