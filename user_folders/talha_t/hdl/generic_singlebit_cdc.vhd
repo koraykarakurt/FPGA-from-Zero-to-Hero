033461c16a1731dce039539cbadf45d307cdc169
Talha Telli <132828233+TalhaTelli427@users.noreply.github.com>
2025-04-07 21:38:37 +0300

    GFIR-64 : Design generic_reset_bridge.vhd and generic_singlebit_cdc.vhd
    GFIR-64 : Add xilinx XDC file and add attribute for xilinx for cdc design

--- user_folders/talha_t/hdl/generic_singlebit_cdc.vhd
+++ user_folders/talha_t/hdl/generic_singlebit_cdc.vhd
@@ -1,9 +1,10 @@
+----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 28.02.2025 20:55:55
+-- Create Date: 07.04.2025 20:11:50
 -- Design Name: 
--- Module Name: generic_cdc - Behavioral
+-- Module Name: generic_singlebit_cdc - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -14,14 +15,17 @@
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity generic_singlebit_cdc is
    generic(
-      VENDOR : string := "efinix";
-      SYNCH_FF_NUMBER: natural range 2 to 6 := 3
+      VENDOR : string := "xilinx";
+      SYNCH_FF_NUMBER: natural range 2 to 6 := 5
    );
     port ( 
       clk      : in std_logic;
@@ -31,48 +35,59 @@ entity generic_singlebit_cdc is
 end generic_singlebit_cdc;
 
 architecture behavioral of generic_singlebit_cdc is
-   signal data_sync_buffer : std_logic_vector (SYNCH_FF_NUMBER-1 downto 0) := (others => '0');
 begin
+
 -- Xilinx attributes IOB, ASYNC_REG ve DONT_TOUCH
 -- Altera attributes USEIOOFF, PRESERVE, ALTERA_ATTRIBUTE (inline constraint)
    xilinx_version: if (VENDOR = "xilinx") generate
+      signal data_sync_buffer : std_logic_vector (SYNCH_FF_NUMBER-1 downto 0) ;
+
       attribute ASYNC_REG : string;
       attribute ASYNC_REG of data_sync_buffer : signal is "true";
       attribute DONT_TOUCH : string;
       attribute DONT_TOUCH of data_sync_buffer : signal is "true";
-      xilinx_data_sync : process(clk)
+	begin
+      xilinx : process(clk)
       begin
-         if (rising_edge(clk)) then
+         if rising_edge(clk) then
             data_sync_buffer <= data_sync_buffer(SYNCH_FF_NUMBER-2 downto 0) & data_in;
          end if;
-      end process xilinx_data_sync;
+      end process xilinx;
+			data_out <= data_sync_buffer(SYNCH_FF_NUMBER-1);
+
    end generate xilinx_version;
 
    intel_version : if (VENDOR = "intel") generate
+         signal data_sync_buffer : std_logic_vector (SYNCH_FF_NUMBER-1 downto 0) := (others => '0');
+
       attribute ALTERA_ATTRIBUTE                     : string;
       attribute ALTERA_ATTRIBUTE of data_sync_buffer : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
-      attribute PRESERVE                             : boolean;
-      attribute PRESERVE of data_sync_buffer         : signal is true;
-
+    
+	  begin
       intel_data_sync : process(clk)
       begin
          if (rising_edge(clk)) then
             data_sync_buffer <= data_sync_buffer(SYNCH_FF_NUMBER-2 downto 0) & data_in;
          end if;
       end process intel_data_sync;
+			data_out <= data_sync_buffer(SYNCH_FF_NUMBER-1);
+
    end generate intel_version;   
    
    efinix_version : if (VENDOR = "efinix") generate
+	  signal data_sync_buffer : std_logic_vector (SYNCH_FF_NUMBER-1 downto 0) := (others => '0');
+
       attribute ASYNC_REG: boolean;
       attribute ASYNC_REG of data_sync_buffer : signal is true;
+	  begin
       efinix_data_sync : process(clk)
       begin
          if (rising_edge(clk)) then
             data_sync_buffer <= data_sync_buffer(SYNCH_FF_NUMBER-2 downto 0) & data_in;
          end if;
       end process efinix_data_sync;
+			data_out <= data_sync_buffer(SYNCH_FF_NUMBER-1);
    end generate efinix_version;
 
-   data_out <= data_sync_buffer(SYNCH_FF_NUMBER-1);
 
 end Behavioral;
